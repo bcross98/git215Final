06b4222fce193233676b91f8c8752f232a6ceeb1
bcross98 <112006189+bcross98@users.noreply.github.com>
2024-02-25 13:15:47 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-//This will grey out the image(s) when you mouseover.
+//This is a really "quick and dirty" way to grayscale images, I learned this from a professor I had at another college when I was still dabbling in the idea of pursuing a degree in the CS field.
 var ovrImg = elem => elem.style.filter = "grayscale(1)";
-//This will restore color on the image(s) when you mouseleave
 var outImg = elem => elem.style.filter = "grayscale(0)";
 
-//This function creates a div with the id of snowblower. Then it runs the snowBlower function below to create a snow falling effect.
+//This function creates a div with the id of snowblower, appends it to the body (doesn't matter where, I defined the "snow" in a separate function), then set a 10 second timeout before fading the snowBlower div out and removing it.
 function makeSnowBlowerDiv() {
     var blowerDiv = $("<div id='snowBlower'></div>");
 
     blowerDiv.appendTo("body");
     
     snowBlower();
+
+    setTimeout(function(){
+        $('#snowBlower').fadeOut(2000, function(){
+            $('#snowBlower').remove();
+        })
+    }, 10000);
 }
 
-//This function removes the snowBlower div, stopping the snow fall
-function removeSnowBlowerDiv() {
-    $('#snowBlower').fadeOut(2000, function(){
-        $('#snowBlower').remove();
-    });
-}
-
-//Function to make "snow"
+//This function creates the "snow". I start with assigning a variable "snowBlower" to a div with the id of snowBlower (created in the function above). Then in the function "createSnow" I assigned a variable named "snow" a new div with the class of "snow". I use the .css function to alter the properties of the "snow". Top is 0 because I want it to start at the top of the page. Left is math.random() which generates a number between 0-1, multiplied by the width of the window to make the "snow" spread the width of the window at random intervals. Opacity is math.random, which is any number between 0-1 (opaque or not). Transform uses the scale property to change the size of the "snow", I'm using math.random() here too, but I set the scale to be between 0-0.5 with the "* 5" and add another 0.5 to it, bringing the scale to between 0.5-1. I use the append function to add it to the snowBlower variable I defined earlier, then I animate it. The animate function is what makes the "snow" look like it's falling, I defined the top and left here to set the endpoint of the snow (with more math.random()). the final Math.random() is to define the amount of time the snow is visible before it's removed by $(this).remove. To finish the whole function I set the snow to fall at an interval of every 100ms.
 function snowBlower() {
 
-    //defines the container where the snow will fall from
     var snowBlower = $('#snowBlower');
 
     function createSnow() {
 
-        //creates a div for the "snow" and randomly defines its sizes and opacity
         var snow = $('<div class="snow"></div>').css({
             top: '0px',
             left: Math.random() * $(window).width(),
@@ -35,10 +31,8 @@ function snowBlower() {
             transform: 'scale(' + (Math.random() * 0.5 + 0.5) + ')'
         });
 
-        //inserts the new "snow" divs into the snowBlower div
         snowBlower.append(snow);
 
-        //animate the snow so that the "snow" fades out as it falls down the page. then this.remove removes them.
         snow.animate({
             top: $(window).height() + 'px',
             left: '+=' + (Math.random() * 200 - 100) + 'px',
@@ -48,14 +42,11 @@ function snowBlower() {
         });
     }
 
-    //an interval for the snow to be generated at, preventing the screen from flooding with "snow"
     setInterval(createSnow, 100);
 }
 
-//this function runs on page load.
+//this function runs on page load. Here I have fadeIn functions to make the page load nicely, I did have to manually set the css function to display a grid otherwise the default is block. The last thing I have in here is the makeSnowBlowerDiv(); function call to start the snow.
 $(document).ready(function() {
-    //.fadeIn will fade the element in from display:none
-    //.css will set the element to a grid
 
     $('header').fadeIn(500);
     $('header').css('display', 'grid');
@@ -64,5 +55,7 @@ $(document).ready(function() {
     $('main').css('display', 'grid');
 
     $('footer').fadeIn(500);
+
+    makeSnowBlowerDiv();
 
 })
